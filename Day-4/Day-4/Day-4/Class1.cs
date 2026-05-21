1bb3cc61f5d9081d28b4b9cbd69fed3e53384dbc
vikash-verma-profile <er.vikashverma@yahoo.com>
2026-05-21 14:58:55 +0530

    added content

---
+++ Day-4/Day-4/Day-4/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4
+{
+    //var vs dyamic
+    internal class Class1
+    {
+        public static void Main1()
+        {
+            //var x = 1;
+            //x = "Vikash";
+            //Console.WriteLine(x);
+
+            dynamic x = 1;
+            x = "Vikash";
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Day-4/Day-4/Day-4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Day_4
+{
+    class Sample<T>
+    {
+        T a, b;
+
+        public Sample(T a,T b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public void Print()
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            Console.WriteLine($"{a}+{b}={(a1+b1)}");
+        }
+
+    }
+    internal class Program
+    {
+
+
+        public static void Sum<T>(T a,T b)
+        {
+            dynamic a1 = a;
+            dynamic b1 = b;
+            Console.WriteLine(a1+b1);
+        }
+        static void Main(string[] args)
+        {
+            //Sum<int>(1, 2);
+            //Sum<string>("Vikash","Verma");
+
+            Sample<int> sample = new Sample<int>(1,2);
+            sample.Print();
+        }
+    }
+}
