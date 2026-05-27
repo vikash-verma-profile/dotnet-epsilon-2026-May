44eaf42607078402d47bb8a74fe118fc919cf3bf
vikash-verma-profile <er.vikashverma@yahoo.com>
2026-05-27 18:15:49 +0530

    comm

---
+++ Day-8/labs/StudentAPI/StudentAPI/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Day-8/labs/StudentAPI/StudentAPI/Models/StudentDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentAPI.Models
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext()
+        {
+                
+        }
+        public StudentDbContext(DbContextOptions<StudentDbContext> option):base(option)
+        {
+                
+        }
+
+        public DbSet<Student> Students { get; set; }
+     
+    }
+}
